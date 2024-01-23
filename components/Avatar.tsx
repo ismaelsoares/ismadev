5386c1d3368ef3ddd8ac38e50eb19a867e38c485
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-23 09:37:27 -0300

    add avatar image

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,4 +1,16 @@
+//next image
+import Image from "next/image";
+
 export const Avatar = () => {
-    return <div>Avatar</div>;
+    return (
+        <div className='hidden xl:flex xl:max-w-none'>
+            <Image
+                src={'/avatar4.png'}
+                width={737}
+                height={678}
+                alt=''
+            />
+        </div>
+    )
 };
 
