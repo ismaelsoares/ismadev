d16f33b9326a05d9e9f1e215d141a298c61343b0
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-24 17:02:04 -0300

    add avatar IA image change

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -5,7 +5,7 @@ export const Avatar = () => {
     return (
         <div className='hidden xl:flex xl:max-w-none'>
             <Image
-                src={'/avatar7.png'}
+                src={'/avatar.png'}
                 width={640}
                 height={578}
                 alt=''
