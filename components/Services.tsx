4c7c0331b483eef09bd7412a246a74e37a95cf80
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-27 13:30:57 -0300

    My Services add

---
+++ components/Services.tsx
@@ -0,0 +1,16 @@
+export const Services = () => {
+   return (
+      <div className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
+         <p className="heading">
+            My <span className="text-yellow-400">Services</span>
+         </p>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] 
+         mx-auto items-center gap-[3rem] mt-[4rem] text-white">
+            <div>1ST</div>
+            <div>2ND</div>
+            <div>3RD</div>
+
+         </div>
+      </div>
+   )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
 import { MobileNav } from "@/components/MobileNav";
 import { Nav } from "@/components/Nav";
+import { Services } from "@/components/Services";
 import { useState } from "react";
 
 const HomePage = () => {
@@ -16,6 +17,7 @@ const HomePage = () => {
             <Hero />
             <div className="relative z-[30]">
                <About />
+               <Services />
             </div>
          </div>
       </div>
