815055d9e61675fae9756caf237276cc6649ca5a
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-26 21:48:33 -0300

    Add text initial home

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,13 @@ import { Particle } from "./Particle"
 
 export const Hero = () => {
     return (
-        <div className="h-[88vh] vg-[url('/images/banner.jpg')] bg-cover bg-center">
+        <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
             <Particle />
+            <div className=" w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+                <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+                    OLÁ, EU SOU <span className="text-yellow-400">ISMAEL!</span>
+                </h1>
+            </div>
         </div>
     )
 }
