360904dbdc66559eb661d5f5278fd00351277758
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-26 23:45:28 -0300

    Add button cv

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,7 @@ import { TextEffect } from "./TextEffect"
 // framer motion
 import { motion } from 'framer-motion';
 import { fadeIn } from "@/variants";
+import { ArrowDownTrayIcon } from "@heroicons/react/20/solid";
 
 export const Hero = () => {
    return (
@@ -26,6 +27,13 @@ export const Hero = () => {
                   ut. Necessitatibus, laboriosam itaque sit laudantium odit commodi!
                   Libero, ad sed consectetur ducimus, nam asperiores quis eligendi sit ipsam eveniet et!
                </p>
+               <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+                  <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
+                   text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+                     <p>Download Cv</p>
+                     <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+                  </button>
+               </div>
             </motion.div>
             <motion.div
                className="w-[600px] h-[600px] hidden bg-[#55e6a5] 
@@ -33,6 +41,7 @@ export const Hero = () => {
                variants={fadeIn('up', 0.5)}
                initial='hidden'
                animate='show'
+               transition={{ duration: 1, ease: 'easeInOut' }}
                exit='hidden'
             >
                <Image
