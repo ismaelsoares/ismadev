4849f74a1a5431f33eb392671805300e08b86104
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-27 11:46:23 -0300

    Change navbar and About section

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,6 @@
+import { ArrowDownTrayIcon } from "@heroicons/react/20/solid"
+import Image from "next/image"
+
 export const About = () => {
    return (
       <div className="bg-[#121121] pb-[3rem] pt-[4rem] md:pt-[8rem]">
@@ -6,10 +9,33 @@ export const About = () => {
                <h1 className="text-[20px] font-bold uppercase text-[#55e6a5] md-[1rem]">SOBRE MIM</h1>
                <h2 className="text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] 
                leading-[2rem] capitalize mb-[3rem] font-bold text-white">
-                  Transforming <span>Visions</span>
+                  Transforming <span className="text-yellow-400">Visions</span>
                </h2>
+               <div className="mb-[3rem] flex items-center md:space-x-10">
+                  <span className="w-[100px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
+                  <p className="text-[19px] text-slate-300 w-[80%]">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                     At atque officiis sunt adipisci quaerat nesciunt laborum consequuntur.
+                     Animi possimus porro incidunt unde deleniti nam voluptatem quas, ut reiciendis fugit architecto.
+                  </p>
+               </div>
+               <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
+                   text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+                  <p>Download Cv</p>
+                  <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+               </button>
             </div>
-            <div></div>
+            <div className="lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative">
+               <Image
+                  src="/images/about.jpg"
+                  alt="user"
+                  layout="fill"
+                  objectFit="contain"
+                  className="relative z-[11] w-[100%] h-[100%] object-contain"
+               />
+               <div className="absolute w-[100%] h-[100%] z-[10] bg-[#55e6a5] top-[-2rem] right-[-2rem]">
+
+               </div>
+            </div>
          </div>
       </div>
    )
