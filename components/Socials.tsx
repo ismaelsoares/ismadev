cddf77b44a46a4c5937994ae7165f5fe6b1e615d
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-27 00:47:05 -0300

    Add socials media icons

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,4 +1,38 @@
+//links
+import Link from "next/link";
+
+// icons
+import {
+   RiLinkedinLine,
+   RiInstagramLine,
+   RiFacebookBoxLine,
+   RiTwitterLine,
+   RiGithubLine,
+   RiWhatsappLine,
+} from 'react-icons/ri';
+
 export const Socials = () => {
-    return <div>Socials</div>;
+   return (
+      <div className="flex items-center text-lg gap-x-5">
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiLinkedinLine />
+         </Link>
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiFacebookBoxLine />
+         </Link>
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiInstagramLine />
+         </Link>
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiTwitterLine />
+         </Link>
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiGithubLine />
+         </Link>
+         <Link href={''} className="transition-all duration-300 hover:text-accent">
+            <RiWhatsappLine />
+         </Link>
+      </div>
+   )
 };
 
