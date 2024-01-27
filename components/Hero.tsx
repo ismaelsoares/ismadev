cbcc2727d1bce1f239622849b369497bd91cd308
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-27 16:32:37 -0300

    Add CV Link Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,8 @@ import { motion } from 'framer-motion';
 import { fadeIn } from "@/variants";
 import { ArrowDownTrayIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 
+const openCV = () => window.open('https://drive.google.com/file/d/1R-lG60KJh_HMD5vT7tzTtzf-b1dlcniY/view?usp=sharing');
+
 export const Hero = () => {
    return (
       <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
@@ -35,8 +37,11 @@ export const Hero = () => {
                   animate='show'
                   exit='hidden'
                >
-                  <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
-                   text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+                  <button
+                     className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
+                     text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2"
+                     onClick={openCV}
+                  >
                      <p>Download Cv</p>
                      <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
                   </button>
