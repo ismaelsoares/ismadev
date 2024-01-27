2a441429cda0811aab08fc5179c51fcb3bdd840a
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-27 01:22:23 -0300

    Add About Area

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,16 @@
 export const About = () => {
    return (
-      <div>About</div>
+      <div className="bg-[#121121] pb-[3rem] pt-[4rem] md:pt-[8rem]">
+         <div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center">
+            <div>
+               <h1 className="text-[20px] font-bold uppercase text-[#55e6a5] md-[1rem]">SOBRE MIM</h1>
+               <h2 className="text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] 
+               leading-[2rem] capitalize mb-[3rem] font-bold text-white">
+                  Transforming <span>Visions</span>
+               </h2>
+            </div>
+            <div></div>
+         </div>
+      </div>
    )
 }
