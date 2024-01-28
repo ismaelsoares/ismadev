b1ca709cf832face3cd55accaac7ba13e020cd1d
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-28 12:56:04 -0300

    Add Services icons and divs

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,3 +1,5 @@
+import { CodeBracketIcon, CommandLineIcon, RocketLaunchIcon } from "@heroicons/react/20/solid"
+
 export const Services = () => {
    return (
       <div className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
@@ -6,9 +8,42 @@ export const Services = () => {
          </p>
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] 
          mx-auto items-center gap-[3rem] mt-[4rem] text-white">
-            <div>1ST</div>
-            <div>2ND</div>
-            <div>3RD</div>
+            <div>
+               <div className="bg-red-700 hover:scale-110 transform transition-all 
+               duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
+                  <CodeBracketIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+                  <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+                     Frontend
+                  </h1>
+                  <p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+                     iste culpa atque porro dolor ullam libero vel qui debitis provident.
+                  </p>
+               </div>
+            </div>
+            <div>
+               <div className="bg-orange-700 hover:scale-110 transform transition-all 
+               duration-300 uppercase font-semibold text-center p-[2rem]">
+                  <RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+                  <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+                     Backend
+                  </h1>
+                  <p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+                     iste culpa atque porro dolor ullam libero vel qui debitis provident.
+                  </p>
+               </div>
+            </div>
+            <div>
+               <div className="bg-blue-700 hover:scale-110 transform transition-all 
+               duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
+                  <CommandLineIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
+                  <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
+                     Fullstack
+                  </h1>
+                  <p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+                     iste culpa atque porro dolor ullam libero vel qui debitis provident.
+                  </p>
+               </div>
+            </div>
 
          </div>
       </div>
