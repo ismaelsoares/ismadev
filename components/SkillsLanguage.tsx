a424378dfd65d3c6b8e81874d8b9cb943f35548b
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-29 16:58:47 -0300

    Update Skills and Socials links

--- components/SkillsLanguage.tsx
+++ components/SkillsLanguage.tsx
@@ -1,3 +1,5 @@
+import { IoLogoCss3, IoLogoHtml5, IoLogoJavascript } from "react-icons/io";
+import { RiJavascriptLine } from "react-icons/ri";
 interface Props {
    skill1: string
    skill2: string
@@ -26,21 +28,24 @@ export const SkillsLanguage = ({
    return (
       <div>
          <div className="relative mb-[3rem]">
-            <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+            <h1 className=" flex p-5 w-[100%] uppercase transition-all hover:text-red-400 bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+               <IoLogoHtml5 className="w-[2rem] h-[2rem]" />
                {skill1}
             </h1>
             <span className={`${level1} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
             </span>
          </div>
          <div className="relative mb-[3rem]">
-            <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+            <h1 className="flex p-5 w-[100%] uppercase transition-all hover:text-blue-400 bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+               <IoLogoCss3 className="w-[2rem] h-[2rem]" />
                {skill2}
             </h1>
             <span className={`${level2} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
             </span>
          </div>
          <div className="relative mb-[3rem]">
-            <h1 className="p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+            <h1 className="flex p-5 w-[100%] uppercase bg-gray-800 rounded-sm text-white text-[20px] font-bold transition-all hover:text-yellow-400">
+               <RiJavascriptLine className="w-[2rem] h-[2rem]" />
                {skill3}
             </h1>
             <span className={`${level3} bottom-0 h-[6px] absolute bg-[#55e6a5]`}>
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -14,19 +14,19 @@ export const Socials = () => {
    return (
       <div className="flex items-center text-lg gap-x-6 text-white mt-4">
          <Link href='https://www.linkedin.com/in/ismaeldoria/' target="_blank" className="">
-            <RiLinkedinLine />
+            <RiLinkedinLine className="w-[2rem] h-[2rem]" />
          </Link>
          <Link href='https://github.com/ismaelsoares' target="_blank" className="transition-all duration-300 hover:text-accent">
-            <RiGithubLine />
+            <RiGithubLine className="w-[2rem] h-[2rem]" />
          </Link>
          <Link href='https://www.facebook.com/ismaelsoaresdoria' target="_blank" className="transition-all duration-300 hover:text-accent">
-            <RiFacebookBoxLine />
+            <RiFacebookBoxLine className="w-[2rem] h-[2rem]" />
          </Link>
          <Link href='https://www.instagram.com/ismael_soares/' target="_blank" className="transition-all duration-300 hover:text-accent">
-            <RiInstagramLine />
+            <RiInstagramLine className="w-[2rem] h-[2rem]" />
          </Link>
          <Link href='https://twitter.com/ismael_doria' target="_blank" className="transition-all duration-300 hover:text-accent">
-            <RiTwitterLine />
+            <RiTwitterLine className="w-[2rem] h-[2rem]" />
          </Link>
       </div>
    )
