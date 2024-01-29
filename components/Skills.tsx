531c6789e23d08a5ac79dc5753f6f9c3f4d74f6a
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-01-29 10:19:57 -0300

    Update skills category

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,7 +1,34 @@
+import { SkillsItem } from "./SkillsItem";
+import { SkillsLanguage } from "./SkillsLanguage";
+
 export const Skills = () => {
    return (
       <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
          <h1 className="heading">Formação & <span className="text-yellow-400">Skills</span></h1>
+         <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
+            <div>
+               <SkillsItem title="React Developer" year="2015-2016" />
+               <SkillsLanguage
+                  skill1="html"
+                  skill2="css"
+                  skill3="javascript"
+                  level1="w-[91%]"
+                  level2="w-[88%]"
+                  level3="w-[80%]"
+               />
+            </div>
+            <div>
+               <SkillsItem title="Next JS Developer" year="2021-2023" />
+               <SkillsLanguage
+                  skill1="React JS"
+                  skill2="Next JS"
+                  skill3="TypeScript"
+                  level1="w-[81%]"
+                  level2="w-[78%]"
+                  level3="w-[85%]"
+               />
+            </div>
+         </div>
       </div>
    );
 }
--- components/SkillsItem.tsx
+++ components/SkillsItem.tsx
@@ -1,5 +1,21 @@
-export const SkillsItem = () => {
+interface Props {
+   title: string
+   year: string
+}
+
+export const SkillsItem = ({ title, year }: Props) => {
    return (
-      <div>SkillsItem</div>
+      <div className="mb-[4rem] md:mb-[8rem]">
+         <span className="px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55e6a5]">
+            {year}
+         </span>
+         <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">
+            {title}
+         </h1>
+         <p className="text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, id in. Velit in minima, atque ea asperiores
+            nisi odit odio reiciendis! Accusamus ullam ex aperiam laboriosam commodi blanditiis inventore optio.
+         </p>
+      </div>
    )
 }
