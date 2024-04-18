cd8312d8d6337ee54451f0e02c326389023a24f2
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-04-18 17:15:51 -0300

    Componentization folders my project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,78 +1,85 @@
-import Image from "next/image"
-import { Particle } from "./Particle"
-import { TextEffect } from "./TextEffect"
-import { Socials } from "./Socials"
+import Image from "next/image";
+import { Particle } from "./Particle";
+import { TextEffect } from "./TextEffect";
+import { Socials } from "./Socials";
 
 // framer motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { fadeIn } from "@/variants";
 import { ArrowDownTrayIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 
-const openCV = () => window.open('https://drive.google.com/file/d/1Wa-wjaJ4hiLaN10SbifqciBnjMOW-85s/view?usp=sharing');
+const openCV = () =>
+  window.open(
+    "https://drive.google.com/file/d/1Wa-wjaJ4hiLaN10SbifqciBnjMOW-85s/view?usp=sharing"
+  );
 
 export const Hero = () => {
-   return (
-      <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
-         <Particle />
-         <div className=" w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
-            <motion.div
-               variants={fadeIn('down', 0.4)}
-               initial='hidden'
-               animate='show'
-               exit='hidden'
-            >
-               <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-                  OLÁ, EU SOU <span className="text-yellow-400">ISMAEL!</span>
-               </h1>
-               <TextEffect />
-               <Socials />
-               <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
-                  Trabalho como desenvolvedor web apaixonado por criar experiências incríveis na web.
-                  Tenho experiência em desenvolvimento front-end e back-end, utilizando tecnologias como HTML, CSS, JavaScript, React, Node.js,
-                  e bancos de dados como MySQL e MongoDB. Estou sempre buscando aprender novas tecnologias e melhorar minhas habilidades para criar aplicações web de alta qualidade.
-                  Estou animado para enfrentar novos desafios e contribuir para projetos inovadores no mundo da web!
-               </p>
-               <motion.div
-                  className="mt-[2rem] flex-col space-y-6 sm:space-y-0 
+  return (
+    <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
+      <Particle />
+      <div className=" w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+        <motion.div
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            OLÁ, EU SOU <span className="text-yellow-400">ISMAEL!</span>
+          </h1>
+          <TextEffect />
+          <Socials />
+          <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
+            Trabalho como desenvolvedor web apaixonado por criar experiências
+            incríveis na web. Tenho experiência em desenvolvimento front-end e
+            back-end, utilizando tecnologias como HTML, CSS, JavaScript, React,
+            Node.js, e bancos de dados como MySQL e MongoDB. Estou sempre
+            buscando aprender novas tecnologias e melhorar minhas habilidades
+            para criar aplicações web de alta qualidade. Estou animado para
+            enfrentar novos desafios e contribuir para projetos inovadores no
+            mundo da web!
+          </p>
+          <motion.div
+            className="mt-[2rem] flex-col space-y-6 sm:space-y-0 
                   sm:flex sm:flex-row items-center sm:space-x-6"
-                  variants={fadeIn('right', 0.6)}
-                  initial='hidden'
-                  animate='show'
-                  exit='hidden'
-               >
-                  <button
-                     className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            <button
+              className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem]
                      text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2"
-                     onClick={openCV}
-                  >
-                     <p>Download Cv</p>
-                     <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
-                  </button>
-                  <button className="flex items-center space-x-2">
-                     <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5]" />
-                     <p className="text-[20px] font-semibold text-white">
-                        Watch The Video
-                     </p>
-                  </button>
-               </motion.div>
-            </motion.div>
-            <motion.div
-               className="w-[600px] h-[600px] hidden bg-[#55e6a5] 
-               relative lg:flex items-center rounded-full"
-               variants={fadeIn('up', 0.5)}
-               initial='hidden'
-               animate='show'
-               transition={{ duration: 1, ease: 'easeInOut' }}
-               exit='hidden'
+              onClick={openCV}
             >
-               <Image
-                  src="/images/avatar01.png"
-                  alt="user"
-                  layout="fill"
-                  className="object-cover rounded-full"
-               />
-            </motion.div>
-         </div>
+              <p>Download Cv</p>
+              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+            </button>
+            <button className="flex items-center space-x-2">
+              <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5]" />
+              <p className="text-[20px] font-semibold text-white">
+                Watch The Video
+              </p>
+            </button>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          className="w-[600px] h-[600px] hidden bg-[#55e6a5] 
+               relative lg:flex items-center rounded-full"
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          exit="hidden"
+        >
+          <Image
+            src="/images/avatar01.png"
+            alt="user"
+            layout="fill"
+            className="object-cover rounded-full"
+          />
+        </motion.div>
       </div>
-   )
-}
+    </div>
+  );
+};
--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -3,8 +3,10 @@ import { SkillsLanguage } from "./SkillsLanguage";
 
 export const Skills = () => {
    return (
-      <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
-         <h1 className="heading">Formação & <span className="text-yellow-400">Skills</span></h1>
+      <div className="pt-16 md:pt-[8rem] pb-[5rem] bg-[#09101a]">
+         <h1 className="heading">
+            Formação & <span className="text-yellow-400">Skills</span>
+         </h1>
          <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
             <div>
                <SkillsItem title="React Developer" year="2015-2016" />
@@ -39,4 +41,4 @@ export const Skills = () => {
          </div>
       </div>
    );
-}
+};
