8323612ae62f345e56ebe768b637059e55fcb89e
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-05-08 15:24:18 -0300

    Add Projects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -2,7 +2,7 @@ import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
   return (
-    <div className="pt-20 pb-20 bg-gray-900">
+    <div id="projects" className="pt-20 pb-20 bg-gray-900">
       <div className="text-center">
         <p className="heading__mini">Recent Works</p>
         <p className="heading__primary">
@@ -10,13 +10,49 @@ export const Projects = () => {
         </p>
       </div>
       <ProjectCard
-        image="/images/react-form-zod.png"
-        link="https://react-form-zod.vercel.app/"
         title="React form zod"
+        link="https://react-form-zod.vercel.app/"
+        image="/images/react-form-zod.png"
         tech1="React"
         tech2="Tailwind"
         tech3="TypeScript"
         tech4="Zod"
+      />
+      <ProjectCard
+        title="Attendee List"
+        link="https://pass-in-web.vercel.app/"
+        image="/images/pass-in-web.png"
+        tech1="React"
+        tech2="Tailwind"
+        tech3="Zod"
+        tech4="TypeScript"
+      />
+      <ProjectCard
+        title="Table Money"
+        link="https://dt-money-git.vercel.app/"
+        image="/images/dt-money.png"
+        tech1="React"
+        tech2="Tailwind"
+        tech3="Zod"
+        tech4="TypeScript"
+      />
+      <ProjectCard
+        title="Expert Notes"
+        link="https://expert-notes-inky.vercel.app/"
+        image="/images/expert-notes.png"
+        tech1="React"
+        tech2="Tailwind"
+        tech3="Zod"
+        tech4="TypeScript"
+      />
+      <ProjectCard
+        title="Dashboard"
+        link="https://openshop-dashboard.vercel.app/"
+        image="/images/openshop-dashboard.png"
+        tech1="React"
+        tech2="Tailwind"
+        tech3="Zod"
+        tech4="TypeScript"
       />
     </div>
   );
