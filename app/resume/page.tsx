d293c551115da2d881657bcdc66b12a4defa65b8
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-05-23 16:35:55 -0300

    change color Scroll Bar and add experiences list on ul li on resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -40,7 +40,7 @@ interface ExperienceEduProps {
   icon: string;
   title: string;
   description: string;
-  item: ItemProps[];
+  items: ItemProps[];
 }
 
 const about: DataProps = {
@@ -93,12 +93,12 @@ interface SkillProps {
   skillList: SkillItemProps[];
 }
 
-const experience: ExperienceEduProps = {
+const experiences: ExperienceEduProps = {
   icon: "/assets/resume/badge.svg",
   title: "My experience",
   description:
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem",
-  item: [
+  items: [
     {
       company: "Web Design Studio",
       position: "Freelance Web Developer",
@@ -119,6 +119,16 @@ const experience: ExperienceEduProps = {
       position: "Front End Developer",
       duration: "2015 - 2016",
     },
+    {
+      company: "Prefeitura",
+      position: "Front End Developer",
+      duration: "2015 - 2016",
+    },
+    {
+      company: "Prefeitura",
+      position: "Front End Developer",
+      duration: "2015 - 2016",
+    },
   ],
 };
 const education: ExperienceEduProps = {
@@ -126,7 +136,7 @@ const education: ExperienceEduProps = {
   title: "My education",
   description:
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem",
-  item: [
+  items: [
     {
       institution: "UniFACEMA",
       degree: "Análise e Desenvolvimento de Sistemas",
@@ -217,11 +227,32 @@ const Resume = () => {
             {/* experience */}
             <TabsContent value="experience" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <h3 className="text-4xl font-bold">{experiences.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {experience.description}
+                  {experiences.description}
                 </p>
-                <ScrollArea className="h-[400px]"></ScrollArea>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experiences.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="size-1.5 rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
               </div>
             </TabsContent>
             {/* education */}
