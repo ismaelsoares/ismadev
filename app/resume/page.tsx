53ec2d8d28b2b6192d4ccad005c853b0b252e90a
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-05-28 17:41:42 -0300

    Fix font education responsive

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -272,7 +272,7 @@ const Resume = () => {
                           className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
                           <span className="text-accent">{item.duration}</span>
-                          <h3 className="lg:text-base md:text-xl max-w-[450px] lg:max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          <h3 className="lg:text-base md:text-xl max-w-[450px] lg:max-w-[260px] min-h-[60px] text-center lg:text-left sm:text-2xl">
                             {item.degree}
                           </h3>
                           <div className="flex items-center gap-3">
