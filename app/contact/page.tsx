5d503b71ede00b850fe703c84b3b1bf671e509ff
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-05-30 14:51:02 -0300

    fix contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -50,7 +50,7 @@ const Contact = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
-      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      className="py-6 sm:py-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
